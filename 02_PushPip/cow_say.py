0f73d05f1e9ed1e3348fd5292450e1e9b32fe233
Peter Chizhov <peter01092000@yandex.ru>
2023-02-21 01:02:06 +0300

    cowfile implemented, stdin bug fixed

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,10 +1,11 @@
 import argparse # command line parsing module
 import cowsay
+import sys
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('message', nargs='?', default = '',
-                    help='message you want cow to say',)
+                    help='message you want cow to say')
 
 preset_names = ['b', 'd', 'g', 'p', 's', 't', 'w', 'y']
 preset_helps = ['initiates Borg mode',
@@ -35,13 +36,14 @@ parser.add_argument('-W', dest='width', default=40, type=int,
 # disable wrapping (-n)
 parser.add_argument('-n', action='store_true',
                     help='disables text wrapping')                     
-# -h? -f cowfile
+# -f cowfile
+parser.add_argument('-f', dest='cowfile', default='default', type=str,
+                    help='set cowfile')
 args = parser.parse_args()
-# print(args.__dict__)
+
 if args.l:
     print(*cowsay.list_cows())
 else:
-
     # -[bdgpstwy]
     no_preset_flag = True
     for preset in preset_names[::-1]:
@@ -51,9 +53,28 @@ else:
 
     if no_preset_flag:
         preset = None
-    print(cowsay.cowsay(message = args.message, 
-                        preset = preset,
-                        eyes = args.eye_string[:2],
-                        tongue = args.tongue_string[:2],
-                        width = args.width,
-                        wrap_text = not args.n))
+        
+    if '/' in args.cowfile:
+        cow_name = 'default'
+        cowfile = args.cowfile
+    else:
+        cow_name = args.cowfile
+        cowfile = None 
+        
+    # stdin if positional message is default
+    if not args.message:
+        lines = []
+        for line in sys.stdin:
+            lines.append(line.strip())
+        message = '\n'.join(lines)
+    else:
+        message = args.message    
+
+    print(cowsay.cowsay(message = message, 
+                    cow = cow_name,
+                    preset = preset,
+                    eyes = args.eye_string[:2],
+                    tongue = args.tongue_string[:2],
+                    width = args.width,
+                    wrap_text = not args.n,
+                    cowfile=cowfile))
